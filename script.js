e1a7cb079f0d273cafd9ae3be13bc49f854534c8
ATedeschi247 <84191428+ATedeschi247@users.noreply.github.com>
2023-03-15 11:02:17 -0400

    Add files via upload
    Worked more on tutorial 3, specifically the sections on Vue attribute binding and dynamic classes.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
 
     data() {            // data to replace the html content
         return {        // this function returns an object and adds properties to the component that can be read by the other methods
+            url: 'https://www.google.com',            
             showBooks: 'true',
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
@@ -12,10 +13,10 @@ const app = Vue.createApp({
             x: 0,
             y: 0,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss'},
-                { title: 'the way of kings', author: 'brandon sanderson'},
-                { title: 'the final empire', author: 'brandon sanderson'}
-            ]
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg', isFav: true},
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80', isFav: false},
+                { title: 'the final empire', author: 'brandon sanderson', img: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', isFav: true}
+            ],
         }
     },
     methods: {
